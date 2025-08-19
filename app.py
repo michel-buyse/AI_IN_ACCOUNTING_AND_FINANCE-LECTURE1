f0b058cef3a776ae2d5d2eaa877789b5465806bf
michel-buyse <michel.buyse@student.kuleuven.be>
2025-08-19 07:48:38 +0000

    1

---
+++ app.py
@@ -0,0 +1,15 @@
+print('starting flask.....')
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/main",methods=["GET","POST"])
+
+def main():
+    #database
+    return(render_template("main.html"))
+
+if __name__ == "__main__":
+    app.run()
+
